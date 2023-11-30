40b3120104f8c61f6c509190dc38e4da1071d107
daniel <daniel.passecker@gmail.com>
2023-11-30 20:19:10 +0100

    fix bug

--- rest/src/main/java/at/codemonkey/rest/personaccount/PersonAccountController.java
+++ rest/src/main/java/at/codemonkey/rest/personaccount/PersonAccountController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
-@RequestMapping("/personaccount")
+@RequestMapping("/personAccount")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true)
 public class PersonAccountController {
